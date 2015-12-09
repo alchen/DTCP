dddb354fb593b3e4c2b104a7112d20e85998eba6
Alexander Chen <alex7c@gmail.com>
2015-12-08 19:04:44 -0800

    Repaired tests

--- spec/static/main.js
+++ spec/static/main.js
@@ -5,7 +5,7 @@ var mkdirp = require('mkdirp');
 var path = require('path');
 var fs = require('fs');
 var app = require('app');
-var ipc = require('ipc');
+var ipc = require('electron').ipcMain;
 var dialog = require('dialog');
 var BrowserWindow = require('browser-window');
 var Menu = require('menu');
@@ -15,6 +15,7 @@ process.port = 0;  // will be used by crash-reporter spec.
 
 app.commandLine.appendSwitch('js-flags', '--expose_gc');
 app.commandLine.appendSwitch('ignore-certificate-errors');
+app.commandLine.appendSwitch('disable-renderer-backgrounding');
 
 ipc.on('message', function (event, arg) {
   event.sender.send('message', arg);
@@ -28,7 +29,8 @@ ipc.on('console.error', function (event, args) {
   console.log.apply(console, args);
 });
 
-ipc.on('process.exit', function (event, code) {
+ipc.on('process.exit', function (event, failures) {
+  var code = !failures ? 0 : 1;
   process.exit(code);
 });
 
@@ -60,103 +62,13 @@ app.on('window-all-closed', function () {
 });
 
 app.on('ready', function () {
-  var template = [
-    {
-      label: 'Atom',
-      submenu: [
-        {
-          label: 'Quit',
-          accelerator: 'CommandOrControl+Q',
-          click: function (item, window) { app.quit(); }
-        },
-      ],
-    },
-    {
-      label: 'Edit',
-      submenu: [
-        {
-          label: 'Undo',
-          accelerator: 'CommandOrControl+Z',
-          selector: 'undo:',
-        },
-        {
-          label: 'Redo',
-          accelerator: 'CommandOrControl+Shift+Z',
-          selector: 'redo:',
-        },
-        {
-          type: 'separator',
-        },
-        {
-          label: 'Cut',
-          accelerator: 'CommandOrControl+X',
-          selector: 'cut:',
-        },
-        {
-          label: 'Copy',
-          accelerator: 'CommandOrControl+C',
-          selector: 'copy:',
-        },
-        {
-          label: 'Paste',
-          accelerator: 'CommandOrControl+V',
-          selector: 'paste:',
-        },
-        {
-          label: 'Select All',
-          accelerator: 'CommandOrControl+A',
-          selector: 'selectAll:',
-        },
-      ]
-    },
-    {
-      label: 'View',
-      submenu: [
-        {
-          label: 'Reload',
-          accelerator: 'CommandOrControl+R',
-          click: function (item, window) { window.restart(); }
-        },
-        {
-          label: 'Enter Fullscreen',
-          click: function (item, window) { window.setFullScreen(true); }
-        },
-        {
-          label: 'Toggle DevTools',
-          accelerator: 'Alt+CommandOrControl+I',
-          click: function (item, window) { window.toggleDevTools(); }
-        },
-      ]
-    },
-    {
-      label: 'Window',
-      submenu: [
-        {
-          label: 'Open',
-          accelerator: 'CommandOrControl+O',
-        },
-        {
-          label: 'Close',
-          accelerator: 'CommandOrControl+W',
-          click: function (item, window) { window.close(); }
-        },
-      ]
-    },
-  ];
-
-  var menu = Menu.buildFromTemplate(template);
-  app.setApplicationMenu(menu);
-
   window = new BrowserWindow({
-    title: 'Electron Tests',
+    title: 'DTCP Tests',
     show: false,
     width: 800,
     height: 600,
-    'web-preferences': {
-      javascript: true  // Test whether web-preferences crashes.
-    },
   });
-  window.loadUrl('file://' + __dirname + '/index.html');
+  window.loadURL('file://' + __dirname + '/index.html');
   window.on('unresponsive', function () {
     var chosen = dialog.showMessageBox(window, {
       type: 'warning',
